89f089ff045c72fc9d90380aa00e4f36636c7361
Jack Park <jackpark@gmail.com>
2023-04-01 11:19:16 -0700

    Now works over http, and gets dbpedia

--- predicatedetector.py
+++ predicatedetector.py
@@ -4,6 +4,12 @@ from antecedents import antecedents
 from predicates import predicates
 from spacy.matcher import PhraseMatcher
 nlp = spacy.load("en_core_web_md")
+import spacy_dbpedia_spotlight
+nlp.add_pipe('dbpedia_spotlight')
+# Note: opentapioca is not all that accurate
+# plus which the repo uses the wrong api URL
+#nlx = spacy.blank('en')
+#nlx.add_pipe('opentapioca')
 
 antMatcher = PhraseMatcher(nlp.vocab)
 predMatcher = PhraseMatcher(nlp.vocab)
@@ -12,36 +18,57 @@ predPatterns = [nlp.make_doc(term) for term in predicates]
 antMatcher.add("prelist", antPatterns)
 predMatcher.add("predlist", predPatterns)
 
-#doc = nlp("Greenhouse gasses have been thought to cause climate change")
-#doc = nlp("Climate change has been thought to have been caused by greenhouse gasses")
-# doc = nlp("Scientists believe  that co2 causes climate change")
-#doc = nlp("Scientists believe that climate change is caused by  carbon dioxide")
-doc = nlp("Scientists have been thought to believe that climate change is caused by  carbon dioxide")
-antMatches = antMatcher(doc)
-predMatches = predMatcher(doc)
+def handleGet(json):
+  txt = json['text']
+  print(txt)
+  doc = nlp(txt)
+  #DBpedia
+  dbps = []
+  for ent in doc.ents:
+    txt = ent.text 
+    kid = ent.kb_id_
+    dbp = ent._.dbpedia_raw_result['@similarityScore']
+    dbps.append((txt, kid, dbp))
+  #Wikidata
+  
+  #wdx = nlx(txt)
+  wkds = []
+  
+  #for edx in wdx.ents:
+  #  txt = edx.text
+  #  kid = edx.kb_id_
+  #  lbl = edx.label_
+  #  dsc = edx._.description
+  #  wkds.append((txt, kid, lbl,dsc))
 
-data = []
-ants = []
-jsn = {}
+  #Predicates
+  antMatches = antMatcher(doc)
+  predMatches = predMatcher(doc)
 
-for mid, start, end in antMatches:
-  jsn['strt'] = start
-  jsn['enx'] = end
-  foo = doc[start:end]
-  jsn['txt'] = str(foo)
-  ants.append(jsn)
+  data = []
+  ants = []
   jsn = {}
-data.append(ants)
 
-preds = []
-jsn = {}
-for mid, start, end in predMatches:
-  jsn['strt'] = start
-  jsn['enx'] = end
-  foo = doc[start:end]
-  jsn['txt'] = str(foo)
-  preds.append(jsn)
+  for mid, start, end in antMatches:
+    jsn['strt'] = start
+    jsn['enx'] = end
+    foo = doc[start:end]
+    jsn['txt'] = str(foo)
+    ants.append(jsn)
+    jsn = {}
+  data.append(ants)
+
+  preds = []
   jsn = {}
-data.append(preds)
+  for mid, start, end in predMatches:
+    jsn['strt'] = start
+    jsn['enx'] = end
+    foo = doc[start:end]
+    jsn['txt'] = str(foo)
+    preds.append(jsn)
+    jsn = {}
+  data.append(preds)
 
-print("DID: ",data)
+  print("DID: ",data)
+  
+  return {'data':data, 'dbp':dbps, 'wkd':wkds}
